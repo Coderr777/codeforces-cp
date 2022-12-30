082889229219838992c085ee0296b271aa9cedc8
Coder <72199521+Coderr777@users.noreply.github.com>
2022-12-30 21:45:02 +0530

    Add files via upload

---
+++ 4a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if (x%2==0&&x!=2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("no");
+    }
+}
