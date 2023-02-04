df78704e5170e186b53e5ea95c8ee1be85dd0549
Coder <72199521+Coderr777@users.noreply.github.com>
2023-02-04 20:54:20 +0530

    Add files via upload

---
+++ 1409a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        /* code */
+        int a,b;
+        cin>>a>>b;
+            
+        {
+            /* code */
+            int c;
+            c=abs(a-b);
+            c=((c+9)/10);
+            cout<<c<<endl;
+        }
+    }
+}
